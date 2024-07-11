99d93ee8ae7a30128871087948c2ef07e78c8bb9
kento <kent.sk.815@gmail.com>
2024-07-11 18:41:27 +0900

    フォルダ名を一部変更

--- home/packages.nix
+++ home/packages.nix
@@ -1,22 +1,23 @@
 { pkgs, ... }: {
-   home.packages = with pkgs; [
-     discord
-     freetube
-     gimp
-     obsidian
-     spotify
-     vscode
-   ] ++ [
-     zip
-     unzip
-     p7zip
-     xdg-utils
-     bottom
-     tldr
-     fd
-     ripgrep
-     bat
-     eza
-     just
-   ]; 
+  home.packages = with pkgs; [
+    discord
+    freetube
+    gimp
+    neovim
+    obsidian
+    spotify
+    vscode
+  ] ++ [
+    zip
+    unzip
+    p7zip
+    xdg-utils
+    bottom
+    tldr
+    fd
+    ripgrep
+    bat
+    eza
+    just
+  ]; 
 }
